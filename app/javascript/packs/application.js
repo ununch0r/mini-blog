e696cc3526637cfcf067fce13cde47098c334cae
ununch0r <bodiastel123@gmail.com>
2020-05-06 01:14:00 +0300

    Blog-1 Add bootstrap to the project

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("bootstrap-sprokets")
+require("popper")
+require("jquery3")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
