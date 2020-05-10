9c8a1f262e7d9e089ee4dc266d800ba30112de44
ununch0r <bodiastel123@gmail.com>
2020-05-10 20:34:35 +0300

    Blog-1 Fix deleting posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
 
-  has_many :taggings
+  has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
 
   validates :title, :summary, :body, presence: true
